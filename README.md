efd7a288b67eacf79a8bbe0c8d2070ff1c6352ae
Krzysztof <programowanie.krzysiek@gmail.com>
2023-05-29 01:21:46 +0200

    v1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Porównanie języków TypeScript oraz JavaScript dla osób zaczynających naukę programowania.
+## Pliki z webinaru - część TypeScript
---
+++ app1.ts
@@ -0,0 +1,30 @@
+// -----------------------------------------------------------
+// [0]
+// -----------------------------------------------------------
+// TypeScript to język programowania, który pozwala pisać
+// kod JavaScript w bardziej rozbudowany i wygodny sposób.
+// Kiedy już masz kod TypeScript, najpierw trzeba skompilować
+// kod do JS, a potem uruchamiasz go jak zwykły kod JS.
+
+// Można w pewnym przybliżeniu traktowac TypeScript jako
+// JavaScript generator. TypeScript dostarcza wygodny sposób
+// pisania kodu, a kompilowanie do kodu JavaScript pozwala
+// uruchamiać go w wielu miejscach zgodnie ze standardem JS.
+
+// Instalacja:
+
+// 1. Zainstaluj Node.js:
+
+// 2. Zainstaluj TS:
+// >> npm install typescript --save-dev
+// >> tsc -v
+// Aktualizacja do najnowszej wersji:
+// >> npm install -g typescript@latest
+
+// 3. Skompiluj kod (nie musisz nic zmieniać w tsconfig.json)
+//    Domyślnie przebuduje wszystkie pliku w root folderze
+//    -w od razu przekompiluje wszystkie zmiany
+// >> npx tsc -w
+
+// 4. Uruchom plik wynikowy JS
+// >> node app1.js
